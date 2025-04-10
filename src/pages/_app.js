784521c5f99e4de744f60930ed069360b0fc4475
PERNOT <mathieu.pernot@cgi.com>
2025-04-10 14:34:19 +0200

    [ADD] foodtrucks page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/App.css';
 import '../styles/test.css';
 import '../styles/globals.css';
+import '../styles/foodtruck.css';
+
 import { Analytics } from "@vercel/analytics/react"
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,17 +10,18 @@ import { useEffect, useState } from 'react';
 
 import Home from './index';
 import About from './test';
+import Foodtruck from './foodtruck';
 
 
 function MyApp() {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    setIsClient(true); 
+    setIsClient(true);
   }, []);
 
   if (!isClient) {
-    return null; 
+    return null;
   }
 
   return (
@@ -27,6 +30,7 @@ function MyApp() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test" element={<About />} />
+          <Route path="/foodtrucks" element={<Foodtruck />} />
         </Routes>
       </Router>
       <Analytics />
