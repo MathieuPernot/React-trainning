47202d15826234e93ad888a8b0e2ca39f763caea
PERNOT <mathieu.pernot@cgi.com>
2025-04-10 10:35:14 +0200

    [UPDATE] changed popup and vlog

--- src/pages/test.js
+++ src/pages/test.js
@@ -26,15 +26,14 @@ const TestPage = () => {
       <section className="recent-articles">
         <h3>Mes derniers articles :</h3>
         <div className="article-preview">
-          <h4>Mon aventure à Paris !</h4>
-          <p>Je vous raconte mon dernier voyage à Paris, c'était incroyable !</p> 
-            <button className = 'popupbouton' onClick={() => setShowPopup(true)}>Lire plus...</button>
-          <Popup show={showPopup} onClose={() => setShowPopup(false)}>
-            <div className="firstpopup">
-                <h2>test1</h2>
-                <p>Ceci est une pop-up React</p>
-            </div>
-          </Popup>
+          <h4>Ma dernière aventure !</h4>
+          <p>Je vous raconte mon dernier vlog, c'était incroyable !</p> 
+          <button
+              className="popupbouton"
+              onClick={() => window.open("https://www.youtube.com/watch?v=J0gss-Zavmo", "_blank")}
+            >
+              Lire plus...
+            </button>         
         </div>
         <div className="article-preview">
           <h4>Ma playlist du mois</h4>
@@ -42,9 +41,38 @@ const TestPage = () => {
           <button className = 'popup' onClick={() => setShowPopup2(true)}>Lire plus...</button>
 
           <Popup show={showPopup2} onClose={() => setShowPopup2(false)}>
-          <div className="secondpopup">
-                <h2>test2</h2>
-                <p>Ceci est une pop-up React</p>
+            <div className="secondpopup">
+              <h2>🎵 Liste de musiques</h2>
+
+              <ul className="music-list">
+                {[
+                  {
+                    id: 1,
+                    title: "Aqua - Barbie Girl",
+                    cover: "https://m.media-amazon.com/images/M/MV5BOWNkMTkyYWItZmNlMS00MTMxLTliNzYtYjRlMjNjZjQ2MzRhXkEyXkFqcGdeQXVyMTI1Mzg0ODA5._V1_FMjpg_UX1000_.jpg",
+                    url: "https://www.youtube.com/watch?time_continue=1&v=ZyhrYis509A&embeds_referring_euri=https%3A%2F%2Fwww.bing.com%2F&embeds_referring_origin=https%3A%2F%2Fwww.bing.com&source_ve_path=Mjg2NjIsMzY4NDIsMjg2NjY"
+                  },
+                  {
+                    id: 2,
+                    title: "O-Zone - Dragostea Din Teï",
+                    cover: "https://img.cdandlp.com/2014/11/imgL/117269774.jpg",
+                    url: "https://www.youtube.com/watch?v=56TKq_qIb40"
+                  },
+                  {
+                    id: 3,
+                    title: "Ziak - Fixette",
+                    cover: "https://th.bing.com/th/id/OIP.bFpgO4hJ3iStXCNX5lqo9wHaEK?rs=1&pid=ImgDetMain",
+                    url: "https://www.youtube.com/watch?v=xk1c02v3U_U"
+                  }
+                ].map((music) => (
+                  <li key={music.id} className="music-item">
+                    <a href={music.url} target="_blank" rel="noopener noreferrer">
+                      <img src={music.cover} alt={music.title} className="music-cover" />
+                      <span className="music-title">{music.title}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
           </Popup>
           
