ce8687fd88f29c82142e8f344950f70cb49a95c6
PERNOT <mathieu.pernot@cgi.com>
2025-04-10 10:17:21 +0200

    [UPDATE] updated test page popups

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/App.css';
 import '../styles/test.css';
 import '../styles/globals.css';
+import '../styles/Popup.css'; 
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import Home from './index';
 import About from './test';
+
 
 function MyApp() {
   const [isClient, setIsClient] = useState(false);
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
+import Popup from './popup';
 
 const TestPage = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [showPopup2, setShowPopup2] = useState(false);
+
   return (
     <div className="profile-container">
-      {/* En-tête de la page */}
       <header className="profile-header">
         <h1>Bienvenue sur le Profil de Xx_TomBartix_xX</h1>
         <p className="profile-tagline">"Je suis le plus cool des cools"</p>
       </header>
 
-      {/* Section photo de profil et informations */}
       <section className="profile-info">
         <div className="profile-image">
           <img src="/MicrosoftTeams-image.png" alt="photo de profil" />
@@ -18,30 +20,37 @@ const TestPage = () => {
           <h2>Tom Tom Tom Sahur</h2>
           <p>Age: 22 ans</p>
           <p>Passionné des states, charismatic et biensur fasion victim</p>
-          <div className="social-links">
-            <a href="#" target="_blank">Twitter</a>
-            <a href="#" target="_blank">Instagram</a>
-            <a href="#" target="_blank">Snapchat</a>
-          </div>
         </div>
       </section>
 
-      {/* Section des articles récents */}
       <section className="recent-articles">
         <h3>Mes derniers articles :</h3>
         <div className="article-preview">
           <h4>Mon aventure à Paris !</h4>
-          <p>Je vous raconte mon dernier voyage à Paris, c'était incroyable !</p>
-          <a href="#">Lire plus...</a>
+          <p>Je vous raconte mon dernier voyage à Paris, c'était incroyable !</p> 
+            <button className = 'popupbouton' onClick={() => setShowPopup(true)}>Lire plus...</button>
+          <Popup show={showPopup} onClose={() => setShowPopup(false)}>
+            <div className="firstpopup">
+                <h2>test1</h2>
+                <p>Ceci est une pop-up React</p>
+            </div>
+          </Popup>
         </div>
         <div className="article-preview">
           <h4>Ma playlist du mois</h4>
           <p>Voici mes morceaux préférés de ce mois-ci, à découvrir absolument !</p>
-          <a href="#">Lire plus...</a>
+          <button className = 'popup' onClick={() => setShowPopup2(true)}>Lire plus...</button>
+
+          <Popup show={showPopup2} onClose={() => setShowPopup2(false)}>
+          <div className="secondpopup">
+                <h2>test2</h2>
+                <p>Ceci est une pop-up React</p>
+            </div>
+          </Popup>
+          
         </div>
       </section>
 
-      {/* Footer */}
       <footer className="profile-footer">
         <p>&copy; 2025 Tom Tom Tom Sahur | Skybloug</p>
       </footer>
@@ -51,4 +60,3 @@ const TestPage = () => {
 
 export default TestPage;
 
-//      <img src="/MicrosoftTeams-image.png" alt="My Image" />
